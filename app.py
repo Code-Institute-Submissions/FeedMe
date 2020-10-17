30a450e2fb7a567e70f53d11b73e48d966839eae
gaspar91 <andreiafg.91@gmail.com>
2020-10-17 21:16:50 +0000

    added login function

--- app.py
+++ app.py
@@ -31,11 +31,11 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
-
+# Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        #check if the username already exists in the database
+        # check if the username already exists in the database
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
@@ -52,6 +52,33 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("You have been successfully registrated!")
     return render_template("register.html")
+
+
+# Log In
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # if the password is incorrect
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for('login'))
+
+        else:
+            # username doesn't exist/is incorrect
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
 
 
 # telling the app how and where to run the application
