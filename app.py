06d5f9ad1cd8811825005a8e06c4689ef0f7c1fc
gaspar91 <andreiafg.91@gmail.com>
2020-10-19 11:43:23 +0000

    added delete function

--- app.py
+++ app.py
@@ -153,6 +153,14 @@ def edit_recipe(recipe_id):
         "edit_recipe.html", recipe=recipe, categories=categories)
 
 
+# Delete a Recipe
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("You're recipe has been deleted")
+    return redirect(url_for("get_recipes"))
+
+
 # telling the app how and where to run the application
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
