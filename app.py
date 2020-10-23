4b9f6dc52918a5b11c3fce3af712d77704dee17e
gaspar91 <andreiafg.91@gmail.com>
2020-10-23 12:32:36 +0000

    successfully added pattern to email field in register page

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def register():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "password": generate_password_hash(request.form.get("password")),
+            "email": request.form.get("email").lower()
         }
         mongo.db.users.insert_one(register)
 
