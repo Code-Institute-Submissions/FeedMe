199a33a7315b35bfce014292aa7e43f29c156c33
gaspar91 <andreiafg.91@gmail.com>
2020-11-12 17:01:56 +0000

    deleted unnecessary code

--- app.py
+++ app.py
@@ -96,12 +96,10 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}!".format(request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = existing_user["username"].lower()
+                flash("Welcome, {}!".format(existing_user["username"]))
+                return redirect(url_for("profile"))
             else:
                 # if the password is incorrect
                 flash("Incorrect Username and/or Password!")
